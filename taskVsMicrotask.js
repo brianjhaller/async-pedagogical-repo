2ae41b3286773050ac11bc7697cd6f8e4c42d7a2
Brian Haller <brianhaller@Brians-MacBook-Pro.local>
2020-01-16 14:31:11 -0500

    showing how micro vs macro tasks resolve in editor/google chrome

---
+++ taskVsMicrotask.js
@@ -0,0 +1,14 @@
+const bar = () => console.log('bar');
+
+const baz = () => console.log('baz');
+
+const foo = () => {
+  console.log('foo');
+  setTimeout(bar, 0);
+  new Promise((resolve, reject) =>
+    resolve('should be right after baz, before bar')
+  ).then(resolve => console.log(resolve));
+  baz();
+}
+
+foo();
